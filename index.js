ecda792de11430751523f720a25a4e6dba58ea52
jayymccants@gmail.com <jayymccants@gmail.com>
2024-03-20 00:01:09 -0400

    Implemented writeCards function to generate thank you messages for each name and event

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+  return gifts;
+}
+wrapGifts(gifts);
+
+const writeCards = (namesArray, eventName) => {
+  const thankYouMessages = [];
+  for (let i = 0; i < namesArray.length; i++) {
+    thankYouMessages.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return thankYouMessages;
+};
+
+const names = ["Jack", "Justine", "Jordan"];
+const event = "birthday party";
+
+const thankYouMessages = writeCards(names, event);
+console.log(thankYouMessages);
+
+function countDown(num) {
+  for (let i = num; i >= 0; i--) {
+    console.log(i);
+  }
+}
+
+countDown(10);
